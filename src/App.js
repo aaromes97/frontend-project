e95133b5bfc81890e44e06a658dd836627181817
André Alcalde <andrealcalde2998@gmail.com>
2022-02-18 01:58:08 +0100

    frontend template and React Router applied

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
 import LoginPage from "./components/auth/LoginPage";
+import AdvertsPage from "./components/adverts/AdvertsPage";
 
 function App() {
   return (
     <div className="App">
-    <LoginPage/>
+      <Router>
+        <Switch>
+
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/adverts">
+            <AdvertsPage />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/adverts" />
+          </Route>
+
+          <Route path="/404">
+            <div>404 | Not Found Page</div>
+          </Route>
+          <Route>
+            <Redirect to="/404" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
