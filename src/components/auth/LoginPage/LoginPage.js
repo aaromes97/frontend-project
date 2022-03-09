5e28f170d997745ada87b7c35e6f592e04a042b5
adridm97 <adri.d.m1997@gmail.com>
2022-03-09 20:42:17 +0100

    enlace en login

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -7,129 +7,21 @@ import { AuthContextConsumer } from "../context";
 import { Link } from "react-router-dom";
 import Loading from "../../Loading";
 
-
 function LoginPage({ onLogin, history }) {
   const [value, setValue] = useState({ name: "", password: "" });
   const [error, setError] = useState(null);
   //para implementar Spinner de Loading
   const [isLoading, setIsLoading] = useState(false);
 
-<<<<<<< HEAD
-  const [saveValue, setSaveValue] = useState(false);
-=======
-	const [value, setValue]= useState({name:'', password:''})
-	const [error, setError] = useState(null)
-	//para implementar Spinner de Loading 
-	const [isLoading, setIsLoading] = useState(false)
-
-	//reseteamos error 
-	const resetError = () => setError(null)
-	
-	const handleChange = event => {
-		setValue(prevState => ({
-			...prevState,
-			[event.target.name]: event.target.value,
-		}));
-	};
-
-    
-
-	const handleSubmit =  async event => {
-		event.preventDefault();
-		setIsLoading(true)
-		resetError();
-		//llamamos al  api - enviamos value
-		try {
-			await login(value);
-			setIsLoading(false)
-			onLogin();
-			history.push('/adverts')
-			console.log(value)
-			
-		} catch (error) {
-			setIsLoading(false)
-			setError(error)
-		}
-	}
-
-<<<<<<< HEAD
->>>>>>> development
-
   //reseteamos error
   const resetError = () => setError(null);
 
-<<<<<<< HEAD
   const handleChange = (event) => {
     setValue((prevState) => ({
       ...prevState,
       [event.target.name]: event.target.value,
     }));
   };
-  const guardarToken = () => {
-    setSaveValue((prevState) => (prevState ? false : true));
-  };
-=======
-		</div>
-	</div>
-=======
-
-	return	<section className="ftco-section">
-		<div className="container">
-		<div className="row justify-content-center">
-		<div className="col-md-6 text-center mx-4">
-		<h2 className="heading-section">ClonePop</h2>
-		</div>
-		</div>
-		<div className="row justify-content-center">
-		<div className="col-md-6 col-lg-5">
-		<div className="login-wrap p-4 p-md-5">
-		<div className="icon d-flex align-items-center justify-content-center">
-		<img className="logoLogin" src={logo} alt="logo"></img>
-		</div>
-		<h3 className="text-center mb-4">Inicia Sesión</h3>
-		<form className="login-form" onSubmit={handleSubmit}>
-		<div className="form-group">	
-		<input className="form-control rounded-left"
-	placeholder="Usuario"
-	type="text" 
-	name="name"
-	value={value.name}
-	onChange={handleChange} />
-		</div>
-		<div className="form-group d-flex">
-		<input className="form-control rounded-left"
-	placeholder="Contraseña"
-	type="password"
-	name="password"
-	value={value.password}
-	onChange={ handleChange}/>
-		</div>
-	<div className="sign up">
-	<Link to="/register">Sign Up</Link>
-	
-	</div>						
-							
->>>>>>> development
-		<div>
-			<Link to="/forgot-password">Forgot your Password?</Link>							
-		</div>					
-		<div className="form-group d-md-flex">
-			
-		<button className="btn btn-primary rounded submit p-3 px-5"
-	type="submit"
-	disabled={!value.name || !value.password}>Login</button>
-	{isLoading && <button className="btn btn-primary rounded submit p-3 px-5" diabled>
-	<Loading>Loading</Loading>					
-	</button>}							
-		</div>
-						</form>
-		</div>
-		</div>
-		</div>
-						{error && <div onClick={resetError} className="error-msg"> <i class="fa fa-times-circle"></i>{error.message }</div>}</div>
-		</section>
-};
->>>>>>> development
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -137,7 +29,7 @@ function LoginPage({ onLogin, history }) {
     resetError();
     //llamamos al  api - enviamos value
     try {
-      await login(value, saveValue);
+      await login(value);
       setIsLoading(false);
       onLogin();
       history.push("/adverts");
@@ -154,7 +46,9 @@ function LoginPage({ onLogin, history }) {
         <div className="row justify-content-center">
           <div className="col-md-6 text-center mx-4">
             <h2 className="heading-section">
-              <Link to="/">ClonePop</Link>{" "}
+              <Link to="/" className="linktoads">
+                ClonePop
+              </Link>
             </h2>
           </div>
         </div>
@@ -187,31 +81,28 @@ function LoginPage({ onLogin, history }) {
                   />
                 </div>
                 <div className="sign up">
-                  <Link to="/register">Sign Up</Link>
+                  <Link to="/register">Crear Usuario</Link>
                 </div>
 
-                <div className="col-12">
-                  <div className="form-check">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value={saveValue}
-                      onChange={guardarToken}
-                    />
-                    <label className="form-check-label">Remember me</label>
-                  </div>
-                </div>
                 <div>
-                  <Link to="/forgot-password">Forgot your Password?</Link>
+                  <Link to="/forgot-password">Has perdido tu contraseña?</Link>
                 </div>
                 <div className="form-group d-md-flex">
                   <button
                     className="btn btn-primary rounded submit p-3 px-5"
                     type="submit"
-                    disabled={isLoading || !value.name || !value.password}
+                    disabled={!value.name || !value.password}
                   >
-                    Log In
+                    Login
                   </button>
+                  {isLoading && (
+                    <button
+                      className="btn btn-primary rounded submit p-3 px-5"
+                      diabled
+                    >
+                      <Loading>Loading</Loading>
+                    </button>
+                  )}
                 </div>
               </form>
             </div>
