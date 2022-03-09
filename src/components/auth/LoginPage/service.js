08ed1f37e5121a1f53bc2b8af01ef057f9819e3b
adridm97 <adri.d.m1997@gmail.com>
2022-03-09 20:19:02 +0100

    service arreglado

--- src/components/auth/LoginPage/service.js
+++ src/components/auth/LoginPage/service.js
@@ -1,22 +1,23 @@
-import client, { removeAuthorizationHeader, setAuthorizationHeader } from '../../../api/client';
-import storage from '../../../utils/storage';
+import client, {
+  removeAuthorizationHeader,
+  setAuthorizationHeader,
+} from "../../../api/client";
+import storage from "../../../utils/storage";
 
 export const login = (credentials, saveValue) => {
-    return client.post('/api/authenticate', credentials).then(({token, name}) => {
-        console.log('login', token)
-        console.log('name', name)
-        setAuthorizationHeader(token);
-        //guardamos el token en LocalStorage si esta el checkbox marcado
-        storage.set('auth', token);
-        
-  
+  return client
+    .post("/api/authenticate", credentials)
+    .then(({ token, name }) => {
+      console.log("login", token);
+      console.log("name", name);
+      setAuthorizationHeader(token);
+      //guardamos el token en LocalStorage si esta el checkbox marcado
+      storage.set("auth", token);
     });
- 
 };
 
-
-
-export const logout = () => Promise.resolve().then(() => {
-    removeAuthorizationHeader()
-    storage.remove('auth');
-});
+export const logout = () =>
+  Promise.resolve().then(() => {
+    removeAuthorizationHeader();
+    storage.remove("auth");
+  });
