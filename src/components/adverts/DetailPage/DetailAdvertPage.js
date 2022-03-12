e75ce6f3e95cd33c9ce3e69d70e9cba9b72c8a55
adridm97 <adri.d.m1997@gmail.com>
2022-03-12 11:36:40 +0100

    divs del detail

--- src/components/adverts/DetailPage/DetailAdvertPage.js
+++ src/components/adverts/DetailPage/DetailAdvertPage.js
@@ -58,7 +58,7 @@ function DetailAdvertPage() {
           {advert.map(
             ({ nombre, precio, descripcion, venta, tags, foto, autor }) => (
               <div className="cardDetail">
-                <div>
+                <div classname="containerDetail">
                   <div className="headerDetail pb-1">
                     <p className="autor pt-2">{autor}</p>
                     <Button className="chat ">Chat para comprar</Button>
