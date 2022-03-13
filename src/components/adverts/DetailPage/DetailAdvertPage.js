f72228f8a8078307b7f1dfd327a3cc4a83340bbf
adridm97 <adri.d.m1997@gmail.com>
2022-03-13 18:05:26 +0100

    reserva y venta hecho

--- src/components/adverts/DetailPage/DetailAdvertPage.js
+++ src/components/adverts/DetailPage/DetailAdvertPage.js
@@ -72,6 +72,23 @@ function DetailAdvertPage() {
       setError(error);
     }
   };
+  const handleVender = async (event) => {
+    event.preventDefault();
+    try {
+      let reservar = new FormData();
+      let vender = new FormData();
+      reservar.append("reservado", false);
+      vender.append("vendido", true);
+      window.location.reload();
+      await editAd(_id, vender);
+    } catch (error) {
+      console.log(error);
+      if (error.status === 401) {
+        return history.push("/login");
+      }
+      setError(error);
+    }
+  };
   // Procedimiento para borrar el anuncio
   // const handleDelete = async () => {
   //     try {
@@ -95,32 +112,49 @@ function DetailAdvertPage() {
     <div>
       {advert && (
         <Layout>
-          {advert.map(
-            ({
-              nombre,
-              precio,
-              descripcion,
-              venta,
-              tags,
-              foto,
-              autor,
-              vendido,
-              reservado,
-            }) => (
-              <div className="cardDetail">
-                <div className="detailContainer">
-                  <div className="headerDetail pb-1">
-                    <p className="autor pt-2">{autor}</p>
-                    {autor === name ? (
-                      <div className="buttondetail-group">
-                        <a className="vender-button btn-grp">
-                          <div>
-                            <img src={vender}></img>
-                          </div>
-                        </a>
-                        {reservado === true ? (
+          <div className="cardDetail">
+            <div className="detailContainer">
+              <div className="headerDetail pb-1">
+                <p className="autor pt-2">{advert[0].autor}</p>
+                {advert[0].autor === name ? (
+                  <div className="buttondetail-group">
+                    {advert[0].vendido === true ? (
+                      <button className="vender-button btn-grp vendido">
+                        <div>
+                          <img src={vender}></img>
+                        </div>
+                      </button>
+                    ) : (
+                      <button
+                        className="vender-button btn-grp"
+                        onClick={handleVender}
+                      >
+                        <div>
+                          <img src={vender}></img>
+                        </div>
+                      </button>
+                    )}
+                    {advert[0].vendido === true ? (
+                      <div>
+                        {advert[0].reservado === true ? (
+                          <button className="guardar-button btn-grp" disabled>
+                            <div>
+                              <img src={reservar}></img>
+                            </div>
+                          </button>
+                        ) : (
+                          <button className="guardar-button btn-grp" disabled>
+                            <div>
+                              <img src={reservar}></img>
+                            </div>
+                          </button>
+                        )}
+                      </div>
+                    ) : (
+                      <div>
+                        {advert[0].reservado === true ? (
                           <button
-                            className="guardar-button btn-grp"
+                            className="guardar-button btn-grp reservado"
                             onClick={handleLiberarReserva}
                           >
                             <div>
@@ -137,51 +171,47 @@ function DetailAdvertPage() {
                             </div>
                           </button>
                         )}
-
-                        <a className="editar-button btn-grp">
-                          <div>
-                            <img src={editar}></img>
-                          </div>
-                        </a>
-                        <a className="borrar-button btn-grp">
-                          <div>
-                            <img src={borrar}></img>
-                          </div>
-                        </a>
                       </div>
-                    ) : (
-                      <Button className="chat ">Chat para comprar</Button>
                     )}
-                  </div>
-                  <img
-                    className="detailFoto"
-                    src={`http://localhost:3001${foto}`}
-                  ></img>
 
-                  <p className="fw-bolder precioDetail">{precio} EUR</p>
-                  <p className="nombreDetail">{nombre}</p>
-                  <div className="d-flex text-dark ventaDetail">
-                    <span>
-                      {venta === true ? <p> Venta </p> : <p> Busco </p>}
-                    </span>
+                    <a className="editar-button btn-grp">
+                      <div>
+                        <img src={editar}></img>
+                      </div>
+                    </a>
+                    <a className="borrar-button btn-grp">
+                      <div>
+                        <img src={borrar}></img>
+                      </div>
+                    </a>
                   </div>
-                  {reservado === true ? (
-                    <p> Reservado </p>
-                  ) : (
-                    <p> No reservado </p>
-                  )}
-                  <div className="descripcionDetail">
-                    <span className="descripcion">{descripcion}</span>
-                  </div>
-                  <div className="bodyDetail">
-                    {tags.map((tag) => (
-                      <div className="tagsDetail">{tag}</div>
-                    ))}
-                  </div>
-                </div>
+                ) : (
+                  <Button className="chat ">Chat para comprar</Button>
+                )}
               </div>
-            )
-          )}
+
+              <img
+                className="detailFoto"
+                src={`http://localhost:3001${advert[0].foto}`}
+              ></img>
+              <p className="fw-bolder precioDetail">{advert[0].precio} EUR</p>
+              <p className="nombreDetail">{advert[0].nombre}</p>
+              <div className="d-flex text-dark ventaDetail">
+                <span>
+                  {advert[0].venta === true ? <p> Venta </p> : <p> Busco </p>}
+                </span>
+              </div>
+
+              <div className="descripcionDetail">
+                <span className="descripcion">{advert[0].descripcion}</span>
+              </div>
+              <div className="bodyDetail">
+                {advert[0].tags.map((tag) => (
+                  <div className="tagsDetail">{tag}</div>
+                ))}
+              </div>
+            </div>
+          </div>
         </Layout>
       )}
     </div>
