cf90e5d003fbb908116555b85f31ae796915dd72
Aaron Méndez <>
2022-03-14 23:49:25 +0100

    Trying to resolve promise on forgot password page

--- src/components/auth/ForgotPassword/ResetPage/service.js
+++ src/components/auth/ForgotPassword/ResetPage/service.js
@@ -6,3 +6,8 @@ export const login = async (credentials, id) => {
 
 	return await client.post('/api/forgot-password/reset/?id='+id, credentials);
 };
+
+export const checkToken = async (id) => {
+
+	return await client.get('/api/forgot-password/check/?id='+id);
+};
--- src/components/auth/PrivateRouteForgotPassword.js
+++ src/components/auth/PrivateRouteForgotPassword.js
@@ -1,32 +1,48 @@
 import { Redirect, Route, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+import {checkToken} from './ForgotPassword/ResetPage/service';
 import client from '../../api/client'
+
+
+async function aa(id) {
+	return await client.get('/api/forgot-password/check/?id='+id);
+}
 
 const PrivateRouteForgotPassword = (props) => {
 
 	const location = useLocation();
 	const queryParams = new URLSearchParams(window.location.search);
 	const id = queryParams.get('id');
-
 	const [resIsChecked, setresIsChecked] = useState(null);
-	console.log("tokenisvalid2:", resIsChecked)
+	//console.log("tokenisvalid2:", resIsChecked)
 
 	useEffect(() => {
-		client.get('/api/forgot-password/check/?id=' + id).then((reset) => {
-			setresIsChecked(reset);
-		});
-		// getLatestAds().then((ads) => {
-		// 	setAds(ads.results.reverse()); // mostramos los ultimos anuncios del array (mas nuevos) primero
-		// });
+		try {	
+
+
+			const promise = aa(id).then((reset) => {
+				console.log("reset:", reset.result)
+				setresIsChecked(reset.result)
+				return reset.result	
+			});
+			console.log("id:",promise)
+			// getLatestAds().then((ads) => {
+			// 	setAds(ads.results.reverse()); // mostramos los ultimos anuncios del array (mas nuevos) primero
+			// });
+
+
+		} catch(err) {
+			console.log("err:",err)
+		}
+
+
 	}, []);
-
-	return resIsChecked ? (
-		<Route {...props} />
-	) : (
-		<Redirect to={{ pathname: "/login", state: { from: location } }} />
-	);
-
+	console.log("resIsChecked:",resIsChecked)
+	if(true) {
+		return (<Route {...props} />);
+	} else {
+		return (<Redirect to={{ pathname: "/login", state: { from: location } }} />);
+	}
 };
 
 export default PrivateRouteForgotPassword;
