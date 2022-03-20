1f95cee9126039510044214bb9e29c0257d237e9
André Alcalde <andrealcalde2998@gmail.com>
2022-03-20 17:31:17 +0100

    chat name displayed

--- src/components/adverts/DetailPage/DetailAdvertPage.js
+++ src/components/adverts/DetailPage/DetailAdvertPage.js
@@ -25,7 +25,7 @@ function DetailAdvertPage({ socket }) {
 
   const part = advertId.split("-");
   const _id = part[1];
-
+  const advertName = part[0];
 
   useEffect(() => {
     getAd(_id)
@@ -35,7 +35,7 @@ function DetailAdvertPage({ socket }) {
   }, [_id]);
 
   const [username, setusername] = useState(name);
-  const [roomname, setroomname] = useState("Chat");
+  const [roomname, setroomname] = useState(advertName);
 
   const handleConfirmDelete = async (event) => {
     event.preventDefault();
